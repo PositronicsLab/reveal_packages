408b04c30440d89636897e154ad10977689c9c3a
James <james@PR2Lab-2.(none)>
2015-09-08 11:58:32 -0400

    updated comments and trimmed out unused, outdated code

--- pendulum/scenario/controller.h
+++ pendulum/scenario/controller.h
@@ -11,16 +11,6 @@
 #include <map>
 
 //-----------------------------------------------------------------------------
-/*
-extern double position_gain;               // positional PD gain
-extern double velocity_gain;               // velocity PD gain
-
-extern double initial_time;                // start time of the simulation
-extern double final_time;                  // goal time to complete the control
-extern double initial_position;            // start position of the pivot joint
-extern double final_position;              // goal position of the pivot joint
-*/
-//-----------------------------------------------------------------------------
 /// returns the initial velocity of all of the joints
 /// @param qd the map returned containing the velocities mapped to the name of 
 ///        the joint
@@ -38,58 +28,8 @@ void get_initial_velocity( std::map<std::string,double>& qd ) {
 /// @param vel the map of joint velocities mapped to the joint name
 /// @param force the map returned containing the joint torques mapped joint name
 void get_control( double t, std::map<std::string,double> pos, std::map<std::string,double> vel, std::map<std::string, double>& force ) {
-/*
-  // initial and goal parameters (provided externally)
-  double q_0 = initial_position;
-  double q_f = final_position;
-  double t_0 = initial_time;
-  double t_f = final_time;
 
-  // gains (provided externally)
-  double Kp = position_gain;
-  double Kv = velocity_gain;
-
-  // compute time exponentials
-  double t3_f = t_f * t_f * t_f;           // final time cubed
-  double t2_f = t_f * t_f;                 // final time squared
-  double t3 = t * t * t;                   // current time cubed
-  double t2 = t * t;                       // current time squared
-
-  // state and control variables
-  double q, q_des, q_err;                  // position
-  double dq, dq_des, dq_err;               // velocity
-  double f;                                // force
-
-  // compute the differential between initial and final state
-  double delta_q = q_f - q_0;
-
-  // map in actual state to state parameters
-  q = pos.find( "pivot_joint" )->second;   // current position
-  dq = vel.find( "pivot_joint" )->second;  // current velocity
- 
-  // compute desired state
-  if( t <= t_f ) {
-    // if time is less than final time, use the cubic trajectory equations
-    // to compute desired position and velocity
-    q_des =(-2.0 * delta_q / t3_f) * t3  +  (3.0 * delta_q / t2_f) * t2  +  q_0;
-    dq_des=(-6.0 * delta_q / t3_f) * t2  +  (6.0 * delta_q / t2_f) * t   +  0;
-  } else {
-    // otherwise, use the goal state as desired position and velocity
-    q_des = q_f;
-    dq_des = 0.0;                          // to balance must have zero velocity
-  }
-
-  // compute error between desired and actual state
-  q_err = q_des - q;                       // position error
-  dq_err = dq_des - dq;                    // velocity error
-
-  // compute force
-  f = Kp * q_err + Kv * dq_err;            // PD force control
-
-  // map out force
-  force.insert( std::pair<std::string,double>("pivot_joint", f) );
-*/
-
+  // insert a null control for the pivot joint
   force.insert( std::pair<std::string,double>("pivot_joint", 0.0) );  
 }
 
--- pendulum/scenario/gazebo/plugin.cpp
+++ pendulum/scenario/gazebo/plugin.cpp
@@ -9,39 +9,16 @@
 #include <controller.h>
 
 //-----------------------------------------------------------------------------
-#define PI 3.14159265358979323846
-
-//-----------------------------------------------------------------------------
-
-// open parameters
-double position_gain = 5.0;
-double velocity_gain = 1.0;
-
-// experiment parameters
-double initial_time = 0.0;
-double final_time = 10.0;
-double initial_position = 0.0;
-double final_position = PI;
-
-//-----------------------------------------------------------------------------
 namespace gazebo {
 //-----------------------------------------------------------------------------
 
 class plugin_c : public ModelPlugin {
 private:
-  /// gazebo callback connection allows Preupdate to be called before a step so
-  /// controls can be applied
-  event::ConnectionPtr _preupdateConnection;
-  /// gazebo callback connection allows Postupdate to be called after a step so
-  /// the simulation state can be recorded for data generation.
-  //event::ConnectionPtr _postupdateConnection;
+  event::ConnectionPtr _preupdateConnection; //< controls applied before update
 
-  /// the gazebo world pointer allows the simulation to get world state 
-  physics::WorldPtr _world;
-  /// the gazebo model pointer allows the controls to be applied to the arm  
-  physics::ModelPtr _model;
-
-  physics::JointPtr _pivot;  
+  physics::WorldPtr _world;  //< to query world state
+  physics::ModelPtr _model;  //< to query model state
+  physics::JointPtr _pivot;  //< to get state and set controls of pivot joint
 
 public:
   //---------------------------------------------------------------------------
@@ -51,21 +28,20 @@ public:
   //---------------------------------------------------------------------------
   /// Destructor
   virtual ~plugin_c( void ) {
-    // remove the gazebo callbacks
+    // unregister the update callback
     event::Events::DisconnectWorldUpdateBegin( _preupdateConnection );
-    //event::Events::DisconnectWorldUpdateBegin( _postupdateConnection );
   }
 
   //---------------------------------------------------------------------------
   /// validates that the scenario loaded matches the references expected and
-  /// sets all the helper link and joint references for ease of use later
+  /// sets any link and joint references for ease of use later
   /// @return true if validation succeeded OR false if validation failed
   bool validate( void ) {
+    // locate the pivot joint in the model
     _pivot = _model->GetJoint( "pivot_joint" );
 
     // if any of the pointers are nothing then validation fails
-    if( !( _world && _model && _pivot ) )
-      return false;
+    if( !( _world && _model && _pivot ) ) return false;
 
     // otherwise the configuration is valid
     return true;
@@ -75,7 +51,7 @@ public:
   /// Initializes the controller.  Fulfills the gazebo ModelPlugin interface
   virtual void Load( physics::ModelPtr model, sdf::ElementPtr sdf ) {
 
-    // get the model and world references
+    // get model and world references
     _model = model;
     _world = model->GetWorld();
 
@@ -87,47 +63,40 @@ public:
     }
 
     // get the starting velocity for the joints from the controller
-    std::map<std::string, double> qd;
-    get_initial_velocity( qd );
+    std::map<std::string, double> dq;
+    get_initial_velocity( dq );
   
     // set the starting velocity for the joints from the controller data
-    _pivot->SetVelocity(0, qd.find("pivot_joint")->second);
+    _pivot->SetVelocity( 0, dq.find("pivot_joint")->second );
 
-    // register the gazebo callbacks.
+    // register the gazebo callback
     _preupdateConnection = event::Events::ConnectWorldUpdateBegin(
       boost::bind( &plugin_c::Preupdate, this ) );
-    //_postupdateConnection = event::Events::ConnectWorldUpdateEnd(
-    //  boost::bind( &plugin_c::Postupdate, this ) );
 
     // -- FIN --
     std::cerr << "controller has initialized" << std::endl;
-  
   }
 
   //---------------------------------------------------------------------------
-  /// Registered as the WorldUpdateBegin callback function for the ModelPlugin.
-  /// Computes the controls to be applied to the arm using the arm controller 
-  /// and applies those controls to corresponding joints
+  /// Registered as the Gazebo WorldUpdateBegin function for the ModelPlugin.
+  /// Gets current state from the updated model, computes controls by calling
+  /// the controller, and applies those controls to corresponding joints.
   virtual void Preupdate( ) {
 
-    // get the current time
-    //double t = Reveal::Sim::Gazebo::helpers_c::sim_time( _world );
-    double t = _world->GetSimTime().Double();
+    double t = _world->GetSimTime().Double(); // get the current virtual time 
 
-    std::map<std::string, double> q;
-    std::map<std::string, double> qd;
-    std::map<std::string, double> u;
+    std::map<std::string, double> q;          // map for position state
+    std::map<std::string, double> dq;         // map for velocity state
+    std::map<std::string, double> u;          // map for controls
 
-    // map in the positions
+    // map in the positions relevant to control code
     q.insert( std::pair<std::string,double>("pivot_joint", _pivot->GetAngle(0).Radian() ) );
 
-    // map in the velocities
-    qd.insert( std::pair<std::string,double>("pivot_joint", _pivot->GetVelocity(0) ) );
+    // map in the velocities relevant to control code
+    dq.insert( std::pair<std::string,double>("pivot_joint", _pivot->GetVelocity(0) ) );
 
-    // compute controls using the arm controller
-    get_control( t, q, qd, u );
-
-    double f = u.find("pivot_joint")->second;
+    // let controller compute controls
+    get_control( t, q, dq, u );
 
     // apply the forces
     _pivot->SetForce( 0, u.find("pivot_joint")->second );
