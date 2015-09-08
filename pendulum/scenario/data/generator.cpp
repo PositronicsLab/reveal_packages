1868360534a78d33d84e38d89f583b4633a0787c
James <james@PR2Lab-2.(none)>
2015-09-08 15:36:43 -0400

    added extensive documentation to pendulum data generator

--- pendulum/scenario/data/generator.cpp
+++ pendulum/scenario/data/generator.cpp
@@ -2,7 +2,7 @@
 #define ODEINT_RUNGEKUTTA_STEPPER  // which stepper to use
 //#define ODEINT_EULER_STEPPER
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 
 #include "pendulum.h"
 
@@ -26,67 +26,89 @@
 #include <reveal/db/database.h>
 #endif  // DB_DIRECT_INSERT
 
-//-----------------------------------------------------------------------------
-//Constants
+//----------------------------------------------------------------------------
+// Constants
 #define PI 3.14159265359
 
+//----------------------------------------------------------------------------
+// Scenario Defintion Values
 double _sample_rate = 0.001;
 double _start_time = 0.0;
 double _end_time = 10.0;
-//-----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
 // Initial state
 #define PENDULUM_LENGTH    1.0
 #define INITIAL_q          PI / 2.0
 #define INITIAL_dq         0.0
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 // Variables
-pendulum_ptr _pendulum;
+pendulum_ptr _pendulum;                //< the pendulum ODEs
 
 #if defined( ODEINT_V1 )
-stepper_t _stepper;
+stepper_t _stepper;                    //< odeint version 1 stepper structure
 #endif
 
-Reveal::Core::scenario_ptr _scenario;
-Reveal::Core::analyzer_ptr _analyzer;
+Reveal::Core::scenario_ptr _scenario;  //< scenario definition
+Reveal::Core::analyzer_ptr _analyzer;  //< analyzer definition
 
-/// the exporter handles exporting all the scenario data to requisite files
-Reveal::Core::exporter_c _exporter;
+/// exporter writes all scenario data to a compatible Reveal import format
+Reveal::Core::exporter_c _exporter;    
 
 #ifdef DB_DIRECT_INSERT
-/// the local reveal database
-boost::shared_ptr<Reveal::DB::database_c> _db;
+boost::shared_ptr<Reveal::DB::database_c> _db;  //< the local reveal database
 #endif // DB_DIRECT_INSERT
 
-long _sec;
-long _nsec;
+/// Accurate time spec to correct error from inaccurate floating point addition
+long _sec;      //< seconds portion of current time
+long _nsec;     //< nanoseconds portion of current time
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// Reads the currently stored time and returns as a floating point value
+/// @return the current time in a double format
 double get_time( void ) {
   return (double)_sec + ((double)_nsec / (double)1E9);
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// Converts the floating point time step into a more accurate representation,
+/// updates the stored time based on the time step, returns the current time
+/// @param dt the time step to add to the currently stored time
 double update_time( double dt ) {
+  // split the time differentials into whole seconds and whole nanoseconds
   double dt_sec = floor( dt );
   double dt_nsec = dt - dt_sec;
 
+  // convert the floating point time differentials to long differentials
   long nsec_step = (long) (dt_nsec * 1E9);
   long sec_step = (long) dt_sec;
 
+  // increment the stored time using the long differentials
   _nsec += nsec_step;
   _sec += sec_step;
+
+  // if nanoseconds has accrued a second through increment, correct the values
   if( _nsec > 1E9 ) { 
     _nsec -= 1E9;
     _sec += 1;
   }
 
-  return (double)_sec + ((double)_nsec / (double)1E9);
+  // get the newly updated time and return it as a floating point value
+  return get_time();
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// Creates a fixed base link reference
+/// @return a Reveal link defining the fixed base link state in the world frame
 Reveal::Core::link_ptr base_link( void ) {
+  // base is positoned at 0,0,0 and rotated 90 degrees to compensate for model
+  // definition in sdf.  The link is fixed to the world and cannot move.
+
+  // create the link object
   Reveal::Core::link_ptr base( new Reveal::Core::link_c( "base_link" ) );
+
+  // set the link state relative to the world frame
   base->state.linear_x( 0.0 );
   base->state.linear_y( 0.0 );
   base->state.linear_z( 0.0 );
@@ -100,28 +122,43 @@ Reveal::Core::link_ptr base_link( void ) {
   base->state.angular_dx( 0.0 );
   base->state.angular_dy( 0.0 );
   base->state.angular_dz( 0.0 );
+
+  // return the link object
   return base;
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// Creates a joint for the pivot
+/// @return a Reveal joint defining the current state of the pivot joint
 Reveal::Core::joint_ptr pivot_joint( void ) {
+  // pivot rotates based on the current ode computation.  reads the ode state
+  // and writes that state into a reveal joint
+
+  // create the joint object
   Reveal::Core::joint_ptr pivot( new Reveal::Core::joint_c( "pivot_joint" ) );
 
+  // define the number of state and controls needed for this joint
   pivot->state.resize( 1 );          // joint has 1 DOF
   pivot->control.resize( 0 );        // joint uses no controls
 
-  // Note: the model is turned 90 degrees in the scenario world frame from its 
-  // model frame
-  pivot->state.q( 0, _pendulum->q - PI/2.0 );
-  pivot->state.dq( 0, _pendulum->dq );
+  // Note: the model is rotated 90 degrees in the scenario world frame from 
+  // its model frame so the state is adjusted accordingly
+  pivot->state.q( 0, _pendulum->q - PI/2.0 );  // set position
+  pivot->state.dq( 0, _pendulum->dq );         // set velocity
 
+  // return the joint object
   return pivot;
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// Creates a scenario definition
+/// @return the scenario definition for the pendulum scenario
 Reveal::Core::scenario_ptr define_scenario( void ) {
+
+  // create the scenario object
   Reveal::Core::scenario_ptr scenario( new Reveal::Core::scenario_c() );
 
+  // set the scenario parameters
   scenario->id = generate_uuid();
   scenario->package_id = "pendulum";
   scenario->description = "a pendulum acting under gravity alone";
@@ -129,35 +166,51 @@ Reveal::Core::scenario_ptr define_scenario( void ) {
   scenario->sample_start_time = _start_time;
   scenario->sample_end_time = _end_time;
 
+  // return the scenario object
   return scenario;
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// Creates an analyzer definition
+/// @return the analyzer definition for the pendulum scenario
 Reveal::Core::analyzer_ptr define_analyzer( Reveal::Core::scenario_ptr scenario ) {
+
+  // create the analyzer object
   Reveal::Core::analyzer_ptr analyzer( new Reveal::Core::analyzer_c() );
 
+  // set the analyzer parameters
   analyzer->scenario_id = scenario->id;
   analyzer->source_path = "pendulum/analyzers/pivot_angle";
   analyzer->build_path = "pendulum/analyzers/pivot_angle/build";
   analyzer->build_target = "libanalyzer.so";
   analyzer->type = Reveal::Core::analyzer_c::PLUGIN;
 
+  // define the keys and user friendly labels for the analysis
   analyzer->keys.push_back( "t" );
   analyzer->labels.push_back( "Virtual time (s)" );
 
   analyzer->keys.push_back( "delta" );
   analyzer->labels.push_back( "Joint angle error" );
 
+  // return the analyzer object
   return analyzer;
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// Creates a trial definition
+/// @param scenario the scenario definition who will own this trial
+/// @param t the time of the trial
+/// @return the trial definition generated for the current time
 Reveal::Core::trial_ptr define_trial( Reveal::Core::scenario_ptr scenario, double t ) {
+
+  // create the trial object
   Reveal::Core::trial_ptr trial( new Reveal::Core::trial_c() );
 
+  // set the trial parameters
   trial->scenario_id = scenario->id;
   trial->t = t;
 
+  // define all the models for this trial
   Reveal::Core::model_ptr model( new Reveal::Core::model_c( "pendulum" ) );
 
   model->insert( base_link() );
@@ -165,16 +218,25 @@ Reveal::Core::trial_ptr define_trial( Reveal::Core::scenario_ptr scenario, doubl
 
   trial->models.push_back( model );
 
+  // return the trial object
   return trial;
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// Creates a model solution definition
+/// @param scenario the scenario definition who will own this solution
+/// @param t the time of the solution
+/// @return the model solution definition generated for the current time
 Reveal::Core::solution_ptr define_solution( Reveal::Core::scenario_ptr scenario, double t ) {
+
+  // create the model solution object
   Reveal::Core::solution_ptr solution( new Reveal::Core::solution_c( Reveal::Core::solution_c::MODEL ) );
 
+  // set the solution parameters
   solution->scenario_id = scenario->id;
   solution->t = t;
 
+  // define all the models for this solution
   Reveal::Core::model_ptr model( new Reveal::Core::model_c( "pendulum" ) );
 
   model->insert( base_link() );
@@ -182,23 +244,28 @@ Reveal::Core::solution_ptr define_solution( Reveal::Core::scenario_ptr scenario,
 
   solution->models.push_back( model );
 
+  // return the solution object
   return solution;
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// Initializes all objects necessary to generate data
+/// @return returns true if data generation is ready to proceed OR false if 
+///         the operation fails for any reason
 bool init( void ) {
+
   // create the pendulum
   double l = PENDULUM_LENGTH;
   double q_0 = INITIAL_q;
   double dq_0 = INITIAL_dq;
   _pendulum = pendulum_ptr( new pendulum_c( l, q_0, dq_0 ) );
 
-  // time is actually maintained as longs in _sec + _nsec
+  // intialize time which internally maintained as longs for accuracy
   _sec = 0;
   _nsec = 0;
 
-  // initialize integrators
 #if defined( ODEINT_V1 )
+  // initialize integrators
   // ODEINT_V1 requires the integrator to be setup before use
   state_t x( 2 );
   _stepper.adjust_size( x );
@@ -225,6 +292,7 @@ bool init( void ) {
   if( _db->insert( _analyzer ) != Reveal::DB::database_c::ERROR_NONE ) {
     printf( "ERROR: failed to insert analyzer into database\n" );
     _db->close();
+    // Note: if this fails, scenario transaction is not rolled back
     return false;
   }
 #endif // DB_DIRECT_INSERT
@@ -232,22 +300,28 @@ bool init( void ) {
   return true;
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// Cleans up all components that expect a clean exit
 void shutdown( void ) {
 #ifdef DB_DIRECT_INSERT
+  // close the database
   _db->close();
 #endif // DB_DIRECT_INSERT
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// Integrates the pendulum forward and updates subsequent state
+/// @param[in] dt the integration step size
+/// @param[in] t0 the time preceding integration
+/// @param[out] t1 the time after integration has completed
 void step( double dt, double t0, double& t1 ) {
-  state_t x( 2 );
+  state_t x( 2 );             // odeint compatible state vector
 
-  // get the state vector
+  // update the state vector with current state
   x[0] = _pendulum->q;
   x[1] = _pendulum->dq;
 
-  // compute end time t1
+  // compute the end time t1
   t1 = t0 + dt;
 
   // integrate
@@ -257,35 +331,43 @@ void step( double dt, double t0, double& t1 ) {
   boost::numeric::odeint::integrate_adaptive( stepper_t(), *_pendulum.get(), x, t0, t1, dt );
 #endif
 
-  // update the pendulum state data structure
+  // update the pendulum state data structure from the odeint computed state
   _pendulum->q = x[0];
   _pendulum->dq = x[1];
-
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+/// The generator application produces a set of Reveal import compatible files
+/// defining the pendulum scenario for a pendulum acting under gravity with no
+/// control inputs.  The pendulum is started at an angle orthogonal to the 
+/// equilibrium axis and oscillates back and forth for a number of seconds.
 int main( void ) {
 
-  double t, tf, dt;
-  Reveal::Core::trial_ptr trial;
-  Reveal::Core::solution_ptr solution;
+  double t, tf, dt;                    // time variables
+  Reveal::Core::trial_ptr trial;       // the current trial definition
+  Reveal::Core::solution_ptr solution; // the current solution definition
 
+  // initialize all components that support the generator
   if( !init() ) {
     printf( "ERROR: Initialization failed.\nExiting\n" );
     shutdown();
     return 1;
   }
 
+  // initialize local time variables
   t = _start_time;
   dt = _sample_rate;
 
-  // compute initial state
+  // compute initial state; also acts as a solution prototype
   Reveal::Core::solution_ptr initial_state = define_solution( _scenario, t );
 
-  // get a prototype for the trial;  solution prototype is initial_state
+  // get a prototype for the trial; solution prototype is initial_state
   trial = define_trial( _scenario, t );
  
-  // export the scenario framework 
+  // export the scenario framework.
+  // Note: the trial and initial_state content is not actually written, but 
+  // the structure of all models contained in them is used to map fields to
+  // joint and link ids when actual data is available
   bool result = _exporter.write( _scenario, _analyzer, initial_state, trial );
   if( !result ) {
     printf( "ERROR: Failed to write export framework.\nExiting\n" );
@@ -299,20 +381,23 @@ int main( void ) {
   _db->insert( initial_state );
 #endif // DB_DIRECT_INSERT
 
+  // simulate until the end time is reached
   while( t <= _end_time ) {
-    // define trial
+
+    // define the trial for the current time
     trial = define_trial( _scenario, t );
 
-    // integrate
+    // integrate the pendulum state forward an amount of time dt beginning at
+    // time t.  tf is computed (and not necessary to this implementation)
     step( dt, t, tf );
 
-    // update time
+    // update time using a more accurate time method rather than relying on tf
     t = update_time( dt );
 
-    // define solution
+    // define the solution for time t (now corresponds to time tf)
     solution = define_solution( _scenario, t );
 
-    // write trial and solution
+    // write the trial and solution
     _exporter.write( trial );
     _exporter.write( solution );
 #ifdef DB_DIRECT_INSERT
@@ -320,6 +405,7 @@ int main( void ) {
     _db->insert( solution );
 #endif // DB_DIRECT_INSERT
 
+    // output the state for debugging
     std::cout << t << " " << _pendulum->q << " " << _pendulum->dq << std::endl;
   }
 
@@ -328,6 +414,7 @@ int main( void ) {
   _db->close();
 #endif // DB_DIRECT_INSERT
 
+  // report success and exit cleanly
   printf( "Data generation succeeded\n" );
   shutdown();
   return 0;
--- pendulum/scenario/data/pendulum.h
+++ pendulum/scenario/data/pendulum.h
@@ -10,7 +10,9 @@
 #include <vector>
 #include <boost/numeric/odeint.hpp>
 #include <boost/shared_ptr.hpp>
-//-----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// Type definitons
 
 class pendulum_c;
 typedef boost::shared_ptr<pendulum_c> pendulum_ptr;
@@ -31,26 +33,36 @@ typedef boost::numeric::odeint::euler< state_t, double, state_t, double > steppe
 #endif // ODEINT_VERSION
 #endif // ODEINT_STEPPER
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 class pendulum_c {
 private:
-  double _l;
+  double _l;    //< the length of the pendulum arm
 public:
-  double q;
-  double dq;
+  double q;     //< to store the joint angle of the pivot joint
+  double dq;    //< to store the joint velocity of the pivot joint
 
+  /// Parameterized constructor
+  /// @param l the length of the pendulum arm
+  /// @param q_0 the initial joint angle of the pivot joint
+  /// @param dq_0 the initial joint velocity of the pivot joint
   pendulum_c( double l, double q_0, double dq_0 ) {
     _l = l;
     q = q_0;
     dq = dq_0;
   }
 
-  void operator()( const state_t& x, state_t& dx, const double t ) {
-    dx[0] = x[1];
-    dx[1] = -9.8 / _l * sin( x[0] );
+  /// The pendulum's ode computation.  Interface for odeint integrators
+  /// @param[in] x_0 the state before integration where x_0[0] is position and
+  ///            x_0[1] is velocity
+  /// @param[out] x_1 the state after integration where x_1[0] is position and 
+  ///            x_1[1] is velocity
+  /// @param[in] t the virtual time of the integration step
+  void operator()( const state_t& x_0, state_t& x_1, const double t ) {
+    x_1[0] = x_0[1];
+    x_1[1] = -9.8 / _l * sin( x_0[0] );
   }
 };
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 
 #endif // _PENDULUM_H_
